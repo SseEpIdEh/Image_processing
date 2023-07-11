03dbd79d68add9be1bc42a3c508bab26ae320029
sepideh <137654366+SseEpIdEh@users.noreply.github.com>
2023-07-11 15:44:18 -0600

    Update Apply a threshold to the image.py

--- Apply a threshold to the image.py
+++ Apply a threshold to the image.py
@@ -22,3 +22,47 @@ cv2.imshow('img5',thresh3 )
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+#cv2.threshold:
+
+**cv2.threshold is a simple global thresholding method where a fixed threshold value is applied to all pixels in the image.**
+  
+It takes the following parameters:
+  
+src: The input image.
+  
+thresh: The threshold value used for classification.
+                                
+maxval: The maximum value assigned to pixels that exceed the threshold.
+  
+type: The type of thresholding applied, such as binary, truncated, to zero, etc.
+  
+The function converts the input image into a binary image by comparing pixel intensities with the threshold value.
+cv2.adaptiveThreshold:
+
+#cv2.adaptiveThreshold is an adaptive thresholding method where the threshold value is calculated locally based on the image characteristics in the neighborhood of each pixel.
+  
+It takes the following parameters:
+  
+src: The input image.
+  
+maxval: The maximum value assigned to pixels that exceed the threshold.
+  
+adaptiveMethod: The method used to calculate the threshold value.
+  
+thresholdType: The type of thresholding applied, such as binary or inverted binary.
+  
+blockSize: The size of the neighborhood used for threshold calculation.
+  
+C: A constant subtracted from the mean or weighted mean.
+
+The function divides the image into smaller regions (blocks) and calculates a threshold value for each block based on its local characteristics.
+                                                                                             
+This adaptive thresholding allows for better handling of varying lighting conditions or uneven illumination across the image.
+
+                          
+********In summary, cv2.threshold applies a fixed global threshold to the entire image, while cv2.adaptiveThreshold calculates a local threshold for each pixel based on its neighborhood. 
+Thdaptive thresholding method is beneficial when dealing with images containing non-uniform lighting
+conditions or when a single global threshold is not sufficient for accurate image segmentation or object extraction.*******
